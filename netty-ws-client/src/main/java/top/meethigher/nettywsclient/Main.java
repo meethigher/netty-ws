6f5b995405ff14803f09aa78fdd708935202e2b6
meethigher <meethigher@qq.com>
2023-02-23 21:48:22 +0800

    feat: update client

--- netty-ws-client/src/main/java/top/meethigher/nettywsclient/Main.java
+++ netty-ws-client/src/main/java/top/meethigher/nettywsclient/Main.java
@@ -4,6 +4,7 @@ import io.socket.client.IO;
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
 
+import java.util.Date;
 import java.util.concurrent.atomic.AtomicReference;
 
 
@@ -28,14 +29,14 @@ public class Main {
     public static Socket connect(String url) {
         IO.Options options = new IO.Options();
         //配置路径，该路径与服务端的context一致
-        options.path="/ws";
+        options.path = "/ws";
         //websocket协议
         options.transports = new String[]{"websocket"};
-        //失败重试次数
-        options.reconnectionAttempts = 3;
-        //失败重连的时间间隔
+        //失败重试次数，默认是int最大值
+//        options.reconnectionAttempts = 3;
+        //失败重连的时间间隔(ms)，默认是1秒
         options.reconnectionDelay = 1000;
-        //连接超时时间(ms)
+        //连接超时时间(ms)，默认是20秒
         options.timeout = 500;
         Socket socket = null;
         try {
@@ -46,30 +47,34 @@ public class Main {
         return socket;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Socket socket = connect(template);
         AtomicReference<String> message = new AtomicReference<>("");
         socket.on(Socket.EVENT_CONNECT, objects -> {
-            System.out.println("连接成功");
-            System.out.println(socket.connected());
+            System.out.println("连接成功" + new Date());
         });
         socket.on(Socket.EVENT_ERROR, objects -> {
-            System.out.println("连接失败");
-            System.exit(0);
+            System.out.println("连接失败" + new Date());
+        });
+        socket.on(Socket.EVENT_RECONNECT_ATTEMPT, objects -> {
+            System.out.println("尝试重连" + new Date());
+        });
+        socket.on(Socket.EVENT_CONNECT_TIMEOUT, objects -> {
+            System.out.println("连接超时" + new Date());
         });
         //监听消息
         socket.on("morning-meeting", args1 -> message.set((String) args1[0]));
         socket.connect();
-        //发送消息
-        socket.emit("morning-meeting", "我来开会了");
+
         while (true) {
-            if (message.get() != null && message.get().equals("会议结束")) {
-                System.out.println("结束");
-                break;
+            if (socket.connected()) {
+                System.out.println("发送消息");
+                socket.emit("morning-meeting", "我来开会了");
+            }
+            Thread.sleep(1000L);
+            if (message.get().equals("会议结束")) {
+                System.exit(0);
             }
         }
-        socket.disconnect();
-        socket.close();
-        System.exit(0);
     }
 }
